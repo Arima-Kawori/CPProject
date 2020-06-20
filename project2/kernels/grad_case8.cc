1cb9431365356664827ac8d28fba90fd5abeefe2
1700012934 <1700012934@pku.edu.cn>
2020-06-20 21:52:33 +0800

    lty

--- project2/kernels/grad_case8.cc
+++ project2/kernels/grad_case8.cc
@@ -3,7 +3,9 @@
 static float tmp_dB[32];
 static float tmp_dA[2][16];
 void grad_case8(float (&dB)[32], float (&dA)[2][16]) {
-  for (int i=0; i<32; ++i) {
-    dA[i / 16][i % 16] = dA[i / 16][i % 16] + dB[i] * (1);
+  for (int idiv16=0; idiv16<2; ++idiv16) {
+    for (int imod16=0; imod16<16; ++imod16) {
+      dA[idiv16][imod16] = dA[idiv16][imod16] + dB[idiv16 * 16 + imod16] * (1);
+    }
   }
 }
