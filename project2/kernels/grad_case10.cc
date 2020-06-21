b9aa0e7eea810a0904c966cb471d339ce4e18395
Arima-Kawori <982754351@qq.com>
2020-06-21 22:12:48 +0800

    Revert "Merge pull request #3 from Arima-Kawori/lty2"
    This reverts commit 5477642a563aaae693b27a85da018a720931fa68, reversing
    changes made to 98c786adba186e5251a7c54f7d6347c0f7cd9b02.

--- project2/kernels/grad_case10.cc
+++ project2/kernels/grad_case10.cc
@@ -8,16 +8,14 @@ void grad_case10(float (&dA)[8][8], float (&dB)[10][8]) {
       dB[i][j] = dB[i][j] + dA[i][j] * (((1)) * (3.0) / ((3.0) * (3.0)));
     }
   }
-  for (int iadd1=0; iadd1<10; ++iadd1) {
+  for (int i=0; i<8; ++i) {
     for (int j=0; j<8; ++j) {
-if (iadd1 - 1>=0 && iadd1 - 1<8)
-      dB[iadd1][j] = dB[iadd1][j] + dA[iadd1 - 1][j] * (((1)) * (3.0) / ((3.0) * (3.0)));
+      dB[i + 1][j] = dB[i + 1][j] + dA[i][j] * (((1)) * (3.0) / ((3.0) * (3.0)));
     }
   }
-  for (int iadd2=0; iadd2<10; ++iadd2) {
+  for (int i=0; i<8; ++i) {
     for (int j=0; j<8; ++j) {
-if (iadd2 - 2>=0 && iadd2 - 2<8)
-      dB[iadd2][j] = dB[iadd2][j] + dA[iadd2 - 2][j] * (((1)) * (3.0) / ((3.0) * (3.0)));
+      dB[i + 2][j] = dB[i + 2][j] + dA[i][j] * (((1)) * (3.0) / ((3.0) * (3.0)));
     }
   }
 }
--- project2/kernels/grad_case6.cc
+++ project2/kernels/grad_case6.cc
@@ -6,14 +6,12 @@ static float tmp_dB[2][16][7][7];
 void grad_case6(float (&C)[8][16][3][3], float (&dA)[2][8][5][5], float (&dB)[2][16][7][7]) {
   for (int n=0; n<2; ++n) {
     for (int c=0; c<16; ++c) {
-      for (int paddr=0; paddr<7; ++paddr) {
-        for (int qadds=0; qadds<7; ++qadds) {
-          for (int k=0; k<8; ++k) {
-            for (int r=0; r<3; ++r) {
-              for (int s=0; s<3; ++s) {
-if (paddr - r>=0 && paddr - r<5)
-if (qadds - s>=0 && qadds - s<5)
-                dB[n][c][paddr][qadds] = dB[n][c][paddr][qadds] + dA[n][k][paddr - r][qadds - s] * (C[k][c][r][s]);
+      for (int p=0; p<5; ++p) {
+        for (int r=0; r<3; ++r) {
+          for (int q=0; q<5; ++q) {
+            for (int s=0; s<3; ++s) {
+              for (int k=0; k<8; ++k) {
+                dB[n][c][p + r][q + s] = dB[n][c][p + r][q + s] + dA[n][k][p][q] * (C[k][c][r][s]);
               }
             }
           }
